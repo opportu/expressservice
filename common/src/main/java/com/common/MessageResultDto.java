af21a065a7384ae228f0403cdcbbc3447a5a1eda
LiJinbao <2965251870@qq.com>
2020-09-21 15:25:01 +0800

    增加了MessageServiceDto

---
+++ common/src/main/java/com/common/MessageResultDto.java
@@ -0,0 +1,31 @@
+package com.common;
+
+public class MessageResultDto {
+
+    /**
+     * 发送回执ID，可根据该ID在接口QuerySendDetails中查询具体的发送状态。
+     */
+    String bizId;
+
+
+    /**
+     * 请求状态码。
+     *
+     * 返回OK代表请求成功。
+     * 其他错误码详见错误码列表。
+     */
+    String code;
+
+
+    /**
+     * 状态码的描述。
+     */
+    String message;
+
+
+    /**
+     * 请求ID。
+     */
+    String requiredId;
+
+}
