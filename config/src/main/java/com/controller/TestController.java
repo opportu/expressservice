75de3f3fd13729fdc6746ed45c2dde130b8ca57e
LiJinbao <2965251870@qq.com>
2020-11-21 16:14:31 +0800

    spring-config演练

---
+++ config/src/main/java/com/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class TestController {
+
+    @Value("${from}")
+    private String from;
+
+    @Autowired
+    private Environment environment;
+
+    @RequestMapping("/from")
+    public String from() {
+        return this.from;
+    }
+
+    @RequestMapping("/from1")
+    public String from1() {
+        return environment.getProperty("from", "undefined");
+    }
+
+}
